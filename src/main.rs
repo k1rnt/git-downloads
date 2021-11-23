e79f4fd7f8bb1aaaced037b9ffd2d6efae0eb357
k1rnt <eru08tera15mora@gmail.com>
2021-11-23 11:10:40 +0900

    ダウンロードディレクトリの変更検知

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
+extern crate notify;
+
+use notify::{Watcher, RecursiveMode, RawEvent, raw_watcher};
+use std::sync::mpsc::channel;
+
 mod platform;
 
 fn main() {
     println!("used {}", platform::get_name());
-    println!("{}", platform::get_downloads_dir());
+    let watch_dir = platform::get_downloads_dir();
+
+    let (tx, rx) = channel();
+    let mut watcher = raw_watcher(tx).unwrap();
+
+    watcher.watch(&format!("{}", watch_dir), RecursiveMode::Recursive).unwrap();
+    loop {
+        match rx.recv() {
+           Ok(RawEvent{path: Some(path), op: Ok(op), cookie}) => {
+               println!("{:?} {:?} ({:?})", op, path, cookie)
+           },
+           Ok(event) => println!("broken event: {:?}", event),
+           Err(e) => println!("watch error: {:?}", e),
+        }
+    }
 }
